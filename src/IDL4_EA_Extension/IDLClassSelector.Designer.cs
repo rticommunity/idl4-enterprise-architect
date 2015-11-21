cea968c492b99f87b84bf23a9489fdee1286e2d6
Gerardo Pardo <gerardo@rti.com>
2015-11-21 13:11:22 -0800

    Cosmetic improvement to UI

--- src/IDL4_EA_Extension/IDLClassSelector.Designer.cs
+++ src/IDL4_EA_Extension/IDLClassSelector.Designer.cs
@@ -49,7 +49,7 @@
             this.treeViewModelElements.CheckBoxes = true;
             this.treeViewModelElements.Location = new System.Drawing.Point(3, 3);
             this.treeViewModelElements.Name = "treeViewModelElements";
-            this.treeViewModelElements.Size = new System.Drawing.Size(225, 425);
+            this.treeViewModelElements.Size = new System.Drawing.Size(294, 425);
             this.treeViewModelElements.TabIndex = 0;
             this.treeViewModelElements.AfterCheck += new System.Windows.Forms.TreeViewEventHandler(this.treeViewModelElements_AfterCheck);
             this.treeViewModelElements.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeViewModelElements_AfterSelect);
@@ -59,11 +59,11 @@
             this.textBoxIDLOutput.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBoxIDLOutput.Location = new System.Drawing.Point(234, 3);
+            this.textBoxIDLOutput.Location = new System.Drawing.Point(303, 3);
             this.textBoxIDLOutput.Multiline = true;
             this.textBoxIDLOutput.Name = "textBoxIDLOutput";
             this.textBoxIDLOutput.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.textBoxIDLOutput.Size = new System.Drawing.Size(389, 425);
+            this.textBoxIDLOutput.Size = new System.Drawing.Size(475, 425);
             this.textBoxIDLOutput.TabIndex = 0;
             this.textBoxIDLOutput.TextChanged += new System.EventHandler(this.textBoxIDLOutput_TextChanged);
             // 
@@ -74,8 +74,8 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.tableLayoutPanel1.AutoSize = true;
             this.tableLayoutPanel1.ColumnCount = 2;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 36.91275F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 63.08725F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 300F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
             this.tableLayoutPanel1.Controls.Add(this.textBoxIDLOutput, 1, 0);
             this.tableLayoutPanel1.Controls.Add(this.treeViewModelElements, 0, 0);
             this.tableLayoutPanel1.Controls.Add(this.buttonGenerate, 0, 2);
@@ -87,15 +87,14 @@
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 47F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 43F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(626, 521);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(781, 521);
             this.tableLayoutPanel1.TabIndex = 1;
             // 
             // buttonGenerate
             // 
             this.buttonGenerate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
             this.buttonGenerate.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.buttonGenerate.Location = new System.Drawing.Point(66, 481);
+            this.buttonGenerate.Location = new System.Drawing.Point(101, 481);
             this.buttonGenerate.Name = "buttonGenerate";
             this.buttonGenerate.Size = new System.Drawing.Size(98, 37);
             this.buttonGenerate.TabIndex = 1;
@@ -107,7 +106,7 @@
             // 
             this.buttonSave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
             this.buttonSave.Enabled = false;
-            this.buttonSave.Location = new System.Drawing.Point(373, 481);
+            this.buttonSave.Location = new System.Drawing.Point(485, 481);
             this.buttonSave.Name = "buttonSave";
             this.buttonSave.Size = new System.Drawing.Size(110, 37);
             this.buttonSave.TabIndex = 2;
@@ -149,10 +148,11 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(622, 520);
+            this.ClientSize = new System.Drawing.Size(782, 520);
             this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "IDLClassSelector";
             this.Text = "IDLClassSelector";
+            this.TopMost = true;
             this.tableLayoutPanel1.ResumeLayout(false);
             this.tableLayoutPanel1.PerformLayout();
             this.idlGenConfiguration.ResumeLayout(false);
