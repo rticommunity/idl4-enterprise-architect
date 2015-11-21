0b41e5eafef44de954e92407a640249ea885da48
Gerardo Pardo <gerardo@rti.com>
2015-11-20 19:58:45 -0800

    Changed License to Apache 2.0

--- src/IDL4_EA_Extension/TextOutputInterface.cs
+++ src/IDL4_EA_Extension/TextOutputInterface.cs
@@ -1,11 +1,17 @@
 ﻿/*********************************************************************************************
-(c) 2005-2013 Copyright, Real-Time Innovations, Inc.  All rights reserved.    	                             
-RTI grants Licensee a license to use, modify, compile, and create derivative works 
-of the Software.  Licensee has the right to distribute object form only for use with RTI 
-products.  The Software is provided “as is”, with no warranty of any type, including 
-any warranty for fitness for any purpose. RTI is under no obligation to maintain or 
-support the Software.  RTI shall not be liable for any incidental or consequential 
-damages arising out of the use or inability to use the software.
+Copyright 2015 Real-Time Innovations, Inc.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
 **********************************************************************************************/
 
 using System;
